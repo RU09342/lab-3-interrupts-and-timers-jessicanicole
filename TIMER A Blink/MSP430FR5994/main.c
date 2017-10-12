87ff04b780c5973924c6c340a6e4cac6fab3d64b
Jessica <wozniakj7@students.rowan.edulslsgit config --global user.name Jessicagit config --global user.email wozniakj7@students.rowan.edu>
2017-10-12 03:12:16 -0400

    added code for 5994

--- TIMER A Blink/MSP430FR5994/main.c
+++ TIMER A Blink/MSP430FR5994/main.c
@@ -1,12 +1,29 @@
-#include <msp430.h> 
+//Timer A Blink on the MSP430FR5994
+//Jessica Wozniak
+//Created: 10/1/17
+//Last updated: 10/10/17
 
+#include <msp430.h>
 
-/**
- * main.c
- */
-int main(void)
+#define LED1 BIT0                                 //Defines LED1 as BIT0
+
+void main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
-	return 0;
+     WDTCTL = WDTPW | WDTHOLD;                    // stop watchdog timer
+
+        TA0CTL=TASSEL_1+MC_1+TACLR;               //ACLK, UP MODE, Clear
+        P1DIR|=LED1;                              //sets LED1 as output
+        TA0CCTL0 = CCIE;                          //CCR0 interrupt enabled
+        TA0CCR0 = 12000;                          //Sets initial value of TA0CCR0 to 12000, setting an initial delay for toggle
+
+        PM5CTL0 &= ~LOCKLPM5;
+
+        __bis_SR_register(LPM0_bits + GIE);       // Enter LPM0, enable interrupts
 }
+
+// Timer A0 ISR
+#pragma vector=TIMER0_A0_VECTOR
+    __interrupt void Timer_A (void)
+    {
+        P1OUT ^= LED1;                            //Toggles LED1
+    }
